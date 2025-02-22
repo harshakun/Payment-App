15315028459833a73ea7e6c73cc80555b1ed1a53
Kannan4957 <112773766+Kannan4957@users.noreply.github.com>
2025-02-22 15:35:19 +0530

    Update vite.config.js

--- vite-project/vite.config.js
+++ vite-project/vite.config.js
@@ -1,9 +1,10 @@
-
+// vite.config.js
 import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import react from '@vitejs/plugin-react-swc';
 
 export default defineConfig({
   plugins: [react()],
-  base: "/paymentfrontend/",  // Make sure this matches your repo name
+  server: {
+    port: 5173, // Default Vite port
+  },
 });
-
