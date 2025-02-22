dbd0e6bbd8106d0c21023415ba1d16bc08e37247
Kannan4957 <112773766+Kannan4957@users.noreply.github.com>
2025-02-22 15:36:32 +0530

    Update App.jsx

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import './App.css'
-import { HashRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Dashboard from './Dashboard';
 import Signin from './Signin';
 import Signup from './Signup';
@@ -12,7 +12,7 @@ function App() {
     <>
     <Router>
       <Routes>
-        <Route path="/signin" element={<Signin />} />
+        <Route path="/" element={<Signin />} />
         <Route path="/signup" element={<Signup />} />
         <Route path="/dashboard" element={<Dashboard />} />
         <Route path="/transaction" element={<Transaction />} />
