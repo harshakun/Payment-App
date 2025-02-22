05679a08051282b4295907515a0c4e13131261ce
Kannan4957 <112773766+Kannan4957@users.noreply.github.com>
2025-02-22 14:58:58 +0530

    Update App.jsx

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import './App.css'
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import Dashboard from './Dashboard';
 import Signin from './Signin';
 import Signup from './Signup';
