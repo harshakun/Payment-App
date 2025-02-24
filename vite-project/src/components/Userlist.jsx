14d64f56f21c22e851963a10a9bde1a5e9a4ca26
Kannan4957 <112773766+Kannan4957@users.noreply.github.com>
2025-02-24 10:07:32 +0530

    Update Userlist.jsx

--- vite-project/src/components/Userlist.jsx
+++ vite-project/src/components/Userlist.jsx
@@ -1,22 +1,22 @@
-import Button from './Button'
-import Img from './user.png'
-import { useNavigate } from 'react-router-dom'
-export default function Userlist({user}){
-    const navigate=useNavigate();
-    
-    return <div className="flex justify-between">
-        <div className='flex justify-center items-center'>
-            <div className="w-7 h-7 flex justify-center items-center ml-2">
-                <img src={Img} alt="User" />
+import Button from "./Button";
+import Img from "./user.png";
+import { useNavigate } from "react-router-dom";
+
+export default function Userlist({ user }) {
+    const navigate = useNavigate();
+
+    return (
+        <div className="flex items-center justify-between bg-gray-800 p-2 rounded-lg shadow-md w-full">
+            <div className="flex items-center space-x-3">
+                <img className="w-8 h-8 rounded-full" src={Img} alt="User" />
+                <div className="text-white text-sm md:text-lg">{user.firstname}</div>
             </div>
-            <div className="text-center text-white m-2">
-                {user.firstname}
+            <div className="ml-auto"> {/* Ensures button stays aligned to the right */}
+                <Button
+                    text="Send Money"
+                    onclick={() => navigate("/Transaction?id=" + user._id + "&name=" + user.firstname)}
+                />
             </div>
         </div>
-        <div>
-            <Button text="Send Money" onclick={(e)=>{
-                navigate("/Transaction?id="+user._id+"&name="+user.firstname);
-            }}></Button>
-        </div>
-    </div>
+    );
 }
