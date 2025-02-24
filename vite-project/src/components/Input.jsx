0facd697cad06eb6165d79f722774f316f464b5b
Kannan4957 <112773766+Kannan4957@users.noreply.github.com>
2025-02-24 10:06:30 +0530

    Update Input.jsx

--- vite-project/src/components/Input.jsx
+++ vite-project/src/components/Input.jsx
@@ -1,11 +1,20 @@
-import '../App.css'
-export default function Input({label,placeholder,id,onclick}){
-    return <div className='p-2 m-2'>
-        <div className="text-center text-white p-1">
-        <label htmlFor={id}>{label}</label>
+import '../App.css';
+
+export default function Input({ label, placeholder, id, onclick }) {
+    return (
+        <div className="p-2">
+            <div className="text-center text-white p-1">
+                <label htmlFor={id}>{label}</label>
+            </div>
+            <div className="flex justify-center w-full">
+                <input 
+                    className="text-center rounded-lg w-full p-2 text-black border border-gray-300 focus:outline-none focus:ring-2 focus:ring-blue-500" 
+                    type="text" 
+                    id={id} 
+                    placeholder={placeholder} 
+                    onChange={onclick} 
+                />
+            </div>
         </div>
-        <div className='flex justify-center w-full'>
-        <input className="text-center rounded-lg w-full p-1"type="text" id={id} placeholder={placeholder} onChange={onclick}/>
-        </div>
-    </div>
+    );
 }
