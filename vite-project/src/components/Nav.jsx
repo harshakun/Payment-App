f93e21fb909f7fe77f7db2b9abbc60a9277c23ab
Kannan4957 <112773766+Kannan4957@users.noreply.github.com>
2025-02-24 10:07:02 +0530

    Update Nav.jsx

--- vite-project/src/components/Nav.jsx
+++ vite-project/src/components/Nav.jsx
@@ -1,29 +1,22 @@
-import Heading from './Heading';
-import Img from './user.png';
-import Button from './Button';
-import { useNavigate } from 'react-router-dom';
-export default function Nav({username}){
-    const navigate=useNavigate();
-    return <div className='flex justify-between bg-black p-2'>
-        <Heading heading="Payment App"></Heading>
-        <div className="flex justify-center items-center h-full">
-            <div className="text-white">
-                Hello,{username}
+import Heading from "./Heading";
+import Img from "./user.png";
+import Button from "./Button";
+import { useNavigate } from "react-router-dom";
+
+export default function Nav({ username }) {
+    const navigate = useNavigate();
+
+    return (
+        <div className="flex flex-col md:flex-row justify-between items-center bg-black p-4 md:p-2 shadow-lg">
+            <Heading heading="Payment App" />
+            <div className="flex items-center gap-3 mt-2 md:mt-0">
+                <div className="text-white text-sm md:text-lg">Hello, {username}</div>
+                <img className="w-8 h-8 rounded-full" src={Img} alt="User" />
             </div>
-           
-            <div className="w-7 h-7 flex justify-center items-center ml-2">
-                <img src={Img} alt="User" />
-            </div>
-            <div>
-                <Button text="Signin" onclick={()=>{
-                    navigate("/Signin");
-                }}></Button>
-            </div>
-            <div>
-            <Button text="Signup" onclick={()=>{
-                    navigate("/Signup");
-                }}></Button>
+            <div className="flex gap-2 mt-2 md:mt-0">
+                <Button text="Signin" onclick={() => navigate("/Signin")} />
+                <Button text="Signup" onclick={() => navigate("/Signup")} />
             </div>
         </div>
-    </div>
+    );
 }
